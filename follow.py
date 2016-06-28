c3791b28c87c6198a3fcd78e7d30c84d38e649d1
Meet Udeshi <mudeshi1209@gmail.com>
2016-06-29 04:00:40 +0530

    Add template matching to implement follow

--- follow.py
+++ follow.py
@@ -17,6 +17,10 @@ class MotionTracker():
     size_dev_thres = 50
     size_steady_time = 0
 
+    STATE_ID_TEMPL = 1
+    STATE_FOLLOW = 2
+    state = STATE_ID_TEMPL
+
     def __init__(self):
         self.camera = cv2.VideoCapture(0)
 
@@ -27,6 +31,19 @@ class MotionTracker():
                 self.arduino_port = p[0]
         if self.arduino_port:
             self.arduino = serial.Serial(self.arduino_port, 38400)
+
+    def turnCamera(self):
+        if self.center != (0, 0):
+            w, h, _ = self.im.shape
+            delta_theta = self.center[0] - w/2
+            delta_theta *= -120.0 / w
+            print delta_theta
+            if self.arduino:
+                self.arduino.write(str(delta_theta).encode('ascii'))
+                self.frames = []
+                self.center = (0, 0)
+                sleep(1)
+
 
     def get_frame_diff_template(self):
         diff = get_motion_mask(self.frames, 40)
@@ -55,33 +72,43 @@ class MotionTracker():
         else:
             return None
 
+
+    def findTemplate(self):
+        match = cv2.matchTemplate(self.im, self.template, cv2.TM_CCORR_NORMED)
+        mv, Mv, ml, Ml = cv2.minMaxLoc(match)
+        print Ml, Mv
+
+        size = self.template.shape
+        cv2.rectangle(self.im, Ml, (Ml[0]+size[0], Ml[1]+size[1]), [0,255,0])
+
+        if Mv > 0.96:
+            return Ml
+        else:
+            return None
+
+
     def run(self):
         while True:
             s, self.im = self.camera.read()
 
-            self.frames.append(self.im.copy())
+            if self.state == self.STATE_ID_TEMPL:
+                self.frames.append(self.im.copy())
 
-            if len(self.frames) == 3:
-                templ = self.get_frame_diff_template()
-                if templ != None:
-                    cv2.imshow("template", templ)
-                self.frames.pop(0)
+                if len(self.frames) == 3:
+                    self.template = self.get_frame_diff_template()
+                    if self.template != None:
+                        self.state = self.STATE_FOLLOW
+                        self.frames = []
+                    else:
+                        self.frames.pop(0)
+            elif self.state == self.STATE_FOLLOW:
+                loc = self.findTemplate()
+                if loc == None:
+                    self.state = self.STATE_ID_TEMPL
 
             cv2.imshow("Test frame", self.im)
 
-            if self.center != (0,0):
-                w, h, _ = self.im.shape
-                delta_theta = self.center[0] - w/2
-                delta_theta *= -120.0 / w
-                print delta_theta
-                if self.arduino:
-                    self.arduino.write(str(delta_theta).encode('ascii'))
-                    self.frames = []
-                    self.center = (0, 0)
-                    sleep(1)
-
             k = cv2.waitKey(5)
-
             if k & 0xFF == 27:
                 break
 
